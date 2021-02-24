ab055ee56f9ba155e465e319344c09c294896514
hassan9494 <hassanalwan36@gmail.com>
2021-02-24 15:46:00 +0200

    fix login

---
+++ lib/services/sign_in.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+Future<String> signInWithGoogle() async {
+  print('$_auth/////////////////***************');
+  print('1');
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  print('2//$googleSignInAccount');
+  final GoogleSignInAuthentication googleSignInAuthentication =
+  await googleSignInAccount.authentication;
+  print('3//$googleSignInAuthentication');
+
+  final AuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
+  );
+  print('4//$credential');
+
+  final UserCredential authResult = await _auth.signInWithCredential(credential);
+  print('5//$authResult');
+  final User user = authResult.user;
+  print('6//$user');
+
+  assert(!user.isAnonymous);
+  print('7//$user');
+  assert(await user.getIdToken() != null);
+  print('8//$user');
+
+  final User currentUser = await _auth.currentUser;
+  print('9//$currentUser');
+  assert(user.uid == currentUser.uid);
+  print('10//$user');
+
+  return 'signInWithGoogle succeeded: $user';
+}
+
+
+
+
+void signOutGoogle() async{
+  await googleSignIn.signOut();
+
+  print("User Sign Out: ");
+}
